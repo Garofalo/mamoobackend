3bf6d5c23c70e507fe8e87210869e73f52edc044
Owner <garofalo.ronald@gmail.com>
2022-02-01 18:15:57 -0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ IS_PRODUCTION = os.getenv('PRODUCTION') == 'yes'
 
 if IS_PRODUCTION:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 else:
     BASE_DIR = Path(__file__).resolve().parent.parent
 

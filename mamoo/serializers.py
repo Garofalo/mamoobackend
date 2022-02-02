5997fe4c787b6f24700350b88f58eaacf0865758
Owner <garofalo.ronald@gmail.com>
2022-02-02 18:04:31 -0500

    user model

--- mamoo/serializers.py
+++ mamoo/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id',)
 
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):

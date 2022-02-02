8de8bf116551107bbb84cb3622a589331deb9aa6
Owner <garofalo.ronald@gmail.com>
2022-02-02 15:40:49 -0500

    admin

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from mamoo.views import MamooList, ProfileList, UserList
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin
 
 router = routers.DefaultRouter()
 
@@ -11,6 +12,7 @@ router.register('user', UserList)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh-token/', TokenRefreshView.as_view()),
 ]
--- mamoo/serializers.py
+++ mamoo/serializers.py
@@ -19,7 +19,7 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
         
-        fields = ('user','mamoo')
+        fields = '__all__'
 
 
 class MamooSerializer(serializers.HyperlinkedModelSerializer):

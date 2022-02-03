c663dbcac2aeed1dea1f286ad553cbddeb71b283
Owner <garofalo.ronald@gmail.com>
2022-02-02 19:07:09 -0500

    user model

--- mamoo/models.py
+++ mamoo/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE)
     def __str__(self):
-        return self.name
+        return self.user
 
 
 @receiver(post_save, sender=User)
--- mamoo/serializers.py
+++ mamoo/serializers.py
@@ -19,7 +19,7 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
         
-        fields = '__all__'
+        fields = ('pk', 'mamoo')
 
 
 class MamooSerializer(serializers.HyperlinkedModelSerializer):

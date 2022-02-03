4ea38ffd25d016d88ad783ec5daf46e2c0321a49
Owner <garofalo.ronald@gmail.com>
2022-02-03 11:52:40 -0500

    ok

--- config/settings.py
+++ config/settings.py
@@ -38,13 +38,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','mamoo-db.herokuapp.com']
 
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-)
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:3000',
+# )
 # Application definition
 
 INSTALLED_APPS = [

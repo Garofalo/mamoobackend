c6f7e54f5f2182e9ea38a25072f1ede46fbe7b31
Owner <garofalo.ronald@gmail.com>
2022-02-03 00:15:57 -0500

    migrations

--- mamoo/models.py
+++ mamoo/models.py
@@ -20,7 +20,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save(username=sender.username)
+    instance.profile.save()
 
 
 class Mamoo(models.Model):
